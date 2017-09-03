cd48adcd0578db353b1482d224c70c0d57ef2e23
Eric Constantinides <eric@ericconstantinides.com>
2017-09-03 15:55:38 -0700

    Add google analytics and remove crufty css

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,24 @@ import SiteFooter from './site-footer'
 import Portfolio from './portfolio'
 import Resume from './resume'
 
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-50643936-1')
+
+/**
+ * @return {null}
+ */
+function Analytics (props) {
+  ReactGA.set({ page: props.location.pathname + props.location.search })
+  ReactGA.pageview(props.location.pathname + props.location.search)
+  return null
+}
+
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <SiteHeader />
+        <Route path='/' component={Analytics} />
         <Route exact path='/' component={Portfolio} />
         <Route path='/resume' component={Resume} />
         <SiteFooter />
