b1a59f908da14c47d55b8e7b7e0e18e513adde5d
Eric Constantinides <eric@ericconstantinides.com>
2018-06-24 20:35:59 -0700

    Update to pm2 ecosystem to eric2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,11 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-13-56-120-181.us-west-1.compute.amazonaws.com',
-      key: '~/.ssh/TheKeyPairofEric.pem',
+      user: 'eric',
+      host: 'eric2',
       ref: 'origin/master',
       repo: 'git@github.com:ericconstantinides/ericconstantinides.com.react.git',
-      path: '/home/ubuntu/Sites/ericconstantinides.com',
+      path: '/var/www/node/ericconstantinides.com',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
