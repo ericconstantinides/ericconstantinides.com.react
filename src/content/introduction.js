6dab6f740296b996b6a2b1664a7fdee1dda89a0c
Eric Constantinides <eric@ericconstantinides.com>
2019-02-28 19:22:31 -0800

    udpate introduction

---
+++ src/content/introduction.js
@@ -0,0 +1,5 @@
+const introduction = `<p><strong>In a nutshell:</strong> I'm a passionate <strong>Front-end UI Engineer</strong> specializing in <strong>React</strong>, <strong>Redux</strong>, <strong>CSS</strong>, and <strong>Vanilla JS</strong>. Most importantly, I always strive for an <strong>Amazing User Experience</strong>!</p>
+  <p><strong>Most Recently:</strong> I'm a React / UI Engineer at Insureon working on the <a href='https://up.apps.insureon.com/' target='_blank'>Customer Portal Web App</a> using React and JavaScript.</p>
+  <p><strong>Previously:</strong> I was working on <a href='https://www.bottomlessbrunch.com' target='_blank'>BottomlessBrunch.com</a>, a React and Redux based web app. <strong>Before that</strong> I was Lead Front-End Developer at EverSport Media. <strong>Before that</strong> I was a Senior Web Developer at Flickerbox in San Francisco.</p>`
+
+export default introduction
--- src/content/resume.js
+++ src/content/resume.js
@@ -1,3 +1,4 @@
+import introduction from './introduction'
 import skills from './skills'
 import experience from './experience'
 import education from './education'
@@ -13,9 +14,7 @@ const resume = {
     linkedin: 'constantinides'
   },
   canonical: 'this',
-  introduction: `<p><strong>In a nutshell:</strong> I'm am a <strong>Front-end UI Engineer</strong> specializing in <strong>React</strong>, <strong>Redux</strong>, <strong>CSS</strong>, and <strong>Vanilla JS</strong>.</p>
-  <p><strong>Most Recently:</strong> I'm a React / UI Engineer at Insureon working on the Customer Portal Web App using React and JavaScript.</p>
-  <p><strong>Previously:</strong> I was working on <a href='https://www.bottomlessbrunch.com' target='_blank'>BottomlessBrunch.com</a>, a React and Redux based web app. <strong>Before that</strong> I was Lead Front-End Developer at EverSport Media. <strong>Before that</strong> I was a Web Developer at Flickerbox in San Francisco.</p>`,
+  introduction,
   experience,
   skills,
   education
